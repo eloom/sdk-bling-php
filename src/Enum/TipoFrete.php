947d1c9fc30d7c40540227b2afd117c24c852c5a
élOOm <contato@eloom.com.br>
2024-07-29 14:43:13 -0300

    Create TipoFrete.php

---
+++ src/Enum/TipoFrete.php
@@ -0,0 +1,12 @@
+<?php
+
+##eloom.licenca##
+
+namespace Eloom\SdkBling\Enum;
+
+class TipoFrete extends BaseEnum {
+
+	const SEM_TRANSPORTE = 'S';
+	const LOGISTICA_CADASTRADA = 'L';
+	const INSERIDO_MANUALMENTE = 'M';
+}
