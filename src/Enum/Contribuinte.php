e92c7a582ee7a4e8dbc86e4facdad7909e7a7cf3
élOOm <contato@eloom.com.br>
2024-08-02 15:51:31 -0300

    Create Contribuinte.php

---
+++ src/Enum/Contribuinte.php
@@ -0,0 +1,14 @@
+<?php
+
+##eloom.licenca##
+
+namespace Eloom\SdkBling\Enum;
+
+class Contribuinte extends BaseEnum {
+
+	const CONTRIBUINTE_DO_ICMS = 1;
+
+	const CONTRIBUINTE_ISENTO_DE_ICMS = 2;
+
+	const NAO_CONTRIBUINTE = 9;
+}
