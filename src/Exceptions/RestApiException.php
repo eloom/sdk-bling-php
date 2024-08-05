fd3303e8105aee5afddc05f126915ab7da379de6
élOOm <contato@eloom.com.br>
2024-08-05 15:04:37 -0300

    Error Message

--- src/Exceptions/RestApiException.php
+++ src/Exceptions/RestApiException.php
@@ -20,7 +20,7 @@ class RestApiException extends Exception {
 		$this->response = $response;
 		$message = $this->prepareMessage($message);
 		$this->prepareFields();
-		$message = $message . (count($this->fields) > 1 ? implode(' | ', $this->fields) : '');
+		$message = $message . $this->getFieldsErrorsMessage();
 
 		parent::__construct($message, $response ? $response->getStatusCode() : $code);
 	}
@@ -63,75 +63,15 @@ class RestApiException extends Exception {
 	}
 
 	public function getFieldsErrorsMessage(): ?string {
-		$fieldsResult = ' ';
+		$message = '';
 
-		foreach ($this->fields as $field => $data) {
-			$fieldsResult .= $field . ': ' . $data['message'] . ';' . PHP_EOL;
+		if(count($this->fields) > 0) {
+			$message = ' | ';
+			foreach ($this->fields as $key => $data) {
+				$message .= $data['message'] . ';' . PHP_EOL;
+			}
 		}
 
-		return $fieldsResult;
-	}
-
-	public function getErrorMessageCode(): string {
-		return $this->getErrorMessageByCode($this->getCode());
-	}
-
-	/**
-	 * Retorna o response ja tratado
-	 *
-	 * @param $code
-	 * @return string
-	 */
-	public function getErrorMessageByCode($code): string {
-		switch ($code) {
-			case 400:
-			{
-				return 'Requisição Mal Formada';
-			}
-			case 401:
-			{
-				return 'Usuário não autorizado';
-			}
-			case 403:
-			{
-				return 'Acesso não autorizado';
-			}
-			case 404:
-			{
-				return 'Recurso não Encontrado';
-			}
-			case 405:
-			{
-				return 'Operação não suportada';
-			}
-			case 408:
-			{
-				return 'Tempo esgotado para a requisição';
-			}
-			case 409:
-			{
-				return 'Recurso em conflito';
-			}
-			case 413:
-			{
-				return 'Requisição excede o tamanho máximo permitido';
-			}
-			case 415:
-			{
-				return 'Content-type inválido';
-			}
-			case 422:
-			{
-				return 'Não foi possível processar as instruções contidas na requisição';
-			}
-			case 429:
-			{
-				return 'Requisição excede a quantidade máxima de chamadas permitidas à API.';
-			}
-			case 500:
-			{
-				return 'Erro na API';
-			}
-		}
+		return $message;
 	}
 }
---
+++ src/Helper/CityHelper.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Eloom\SdkBling\Helper;
+class CityHelper {
+
+	public function doExchangeForBrasilia(string $uf, string $city) {
+
+	}
+}
